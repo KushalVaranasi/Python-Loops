b60499baa005c664bc5581882a368d7465cc2988
KushalVaranasi <107536562+KushalVaranasi@users.noreply.github.com>
2022-09-25 18:52:26 +0530

    Add files via upload

---
+++ While loop.py
@@ -0,0 +1,9 @@
+def count_down(start_number):
+    current = -1
+    while (current <= start_number):
+        print(start_number)
+        start_number -= 1
+        current += 1
+    print("Zero!")
+
+count_down(5)
