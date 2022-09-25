8dab225e599a5c521c5c3193f553b49bfffd89b4
KushalVaranasi <107536562+KushalVaranasi@users.noreply.github.com>
2022-09-25 18:52:05 +0530

    Delete While loop.py

--- While loop.py
+++
@@ -1,9 +0,0 @@
-def count_down(start_number):
-    current = -1
-    while (current <= start_number):
-        print(start_number)
-        start_number -= 1
-        current += 1
-    print("Zero!")
-
-count_down(-1)
